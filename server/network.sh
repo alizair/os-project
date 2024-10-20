59c52a51c951117baa4d3d78b1b1b0ddcb70d095
Insomiac-Owl <omarkalas2002@gmail.com>
2024-10-20 23:43:48 +0300

    traceroute.sh done
    1. decided to include the log so we could compare results.
    2. didn't change anything in network.sh except making the log file a variable.
    3. created and tested traceroute.sh successfully.

--- server/network.sh
+++ server/network.sh
@@ -19,31 +19,28 @@ fi
 
 client1_ip=$1
 client2_ip=$2
+log_file="./network.log"
 
 for i in {1..3}; do
     echo "pinging client 1 ip: $client1_ip ..."
     if ping -c 3 -W 3 "$client1_ip"; then    #-c 3 means send 3 packets, -W means wait time is 3 seconds
         timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-        echo "$timestamp: Connectivity with $client1_ip is ok" | tee -a ./network.log  #takes input from previous command output and appends (-a) to network.log
+        echo "$timestamp: Connectivity with $client1_ip is ok" | tee -a "$log_file" #takes input from previous command output and appends (-a) to network.log
     else
         timestamp=$(date +"%Y-%m-%d %H:%M:%S")
         echo "Connection with $client1_ip is down at $timestamp"
-        echo "Connection with $client1_ip is down at $timestamp" | tee -a ./network.log
+        echo "Connection with $client1_ip is down at $timestamp" | tee -a "$log_file"
         ./traceroute.sh "$client1_ip"
-        # Uncomment the next line if you want to reboot here, im not sure if its in traceroute or here
-        # sudo reboot
     fi
 
     echo "pinging client 2 ip: $client2_ip ..."
     if ping -c 3 -W 3 "$client2_ip"; then     
         timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-        echo "$timestamp: Connectivity with $client2_ip is ok" | tee -a ./network.log
+        echo "$timestamp: Connectivity with $client2_ip is ok" | tee -a "$log_file"
     else
         timestamp=$(date +"%Y-%m-%d %H:%M:%S")
         echo "Connection with $client2_ip is down at $timestamp"
-        echo "Connection with $client2_ip is down at $timestamp" | tee -a ./network.log
+        echo "Connection with $client2_ip is down at $timestamp" | tee -a "$log_file"
         ./traceroute.sh "$client2_ip"
-        # Uncomment the next line if you want to reboot here, im not sure if its in traceroute or here
-        # sudo reboot
     fi
 done
