5482781ce708884ca4439b8dfcf7557a99125f51
Ali Zair <az2109964@qu.edu.qa>
2024-10-21 22:36:16 +0300

    updated system.sh to display and log the used and free memory and display the physical cores instead of the logical cores

--- server/system.sh
+++ server/system.sh
@@ -8,6 +8,16 @@ df -h | tee -a ./disk_info.log
 
 echo "----------------------------------"
 
+vmstat -s | awk '
+/total memory/ {total=$1}
+/used memory/ {used=$1}
+/free memory/ {free=$1}
+END {
+	printf "Used Memory: %.2fGB (%.2f%%)\n", used/1024/1024, (used/total)*100;
+	printf "Free Memory: %.2fGB (%.2f%%)\n", free/1024/1024, (free/total)*100;
+}' | tee -a ./mem_cpu_info.log
+
+
 # storing the cpu model in a variable
 cpu_model=$(grep -m 1 'model name' /proc/cpuinfo)
 # removing the 'model name' string from the output
@@ -15,5 +25,6 @@ cpu_model=${cpu_model:13}
 echo "CPU Model:" $cpu_model | tee -a ./mem_cpu_info.log
 
 # storing the number of logical cores in a variable
-cores=$(nproc)
-echo "Number of logical cores:" $cores |  tee -a ./mem_cpu_info.log
+cores=$(grep -m 1 'cpu cores' /proc/cpuinfo)
+cores=${cores:12}
+echo "Number of physical cores:" $cores |  tee -a ./mem_cpu_info.log
