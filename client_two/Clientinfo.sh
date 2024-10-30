fe4722e5ab1568714cb96bbcfd592a3056926714
AAlghoul9 <aa2103693@qu.edu.qa>
2024-10-30 21:52:40 +0300

    clientinfo updt

--- client_two/Clientinfo.sh
+++ client_two/Clientinfo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 log_file="process_info.log"
+username="client2"
+server_ip="10.10.10.131"
+
+if ! command -v ssh &>/dev/null || ! command -v scp &>/dev/null; then
+    echo "ssh or scp not found. Installing..."
+    sudo apt update
+    sudo apt install -y ssh
+fi
+
 echo "Process Information on $(date):" > $log_file
 
 # Process tree of all currently running processes
@@ -19,5 +28,8 @@ free -h >> $log_file
 echo -e "\nTop 5 Resource-Consuming Processes:" >> $log_file
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 6 >> $log_file
 
-# Securely copy the log file to VM2
-scp "$file_name" username@VM2_IP:/path/to/destination/folder/
+# Making sure the logs directory exists on the server
+ssh $username@$server_ip "mkdir -p logs"
+
+# Securely copy the log file to VM2 using SSH private key
+scp "$log_file" $username@$server_ip:/home/client2/logs
