a277083fa363ad9cf7dfcbecccc84d7e6480f0fc
Ali Zair <az2109964@qu.edu.qa>
2024-10-30 20:22:13 +0300

    update the script to use ssh private key for authentication instead of password

--- client_one/login.sh
+++ client_one/login.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-#for security reasons we should only allow root to be able to read and write
-SERVER_IP="10.0.2.7"
+SERVER_IP="10.10.10.131"
 SFTP_USER="client1"
-SFTP_PASS="1234"
 INVALID_ATTEMPTS_LOG="invalid_attempts.log"
 CLIENT_LOG="client_timestamp_invalid_attempts.log"
 MAX_ATTEMPTS=3
 ATTEMPT_COUNT=0
 
 # Check and install dependencies 
-if ! command -v ssh &> /dev/null; then
-    echo "ssh not found. Installing..."
+if ! command -v ssh &>/dev/null || ! command -v sftp &>/dev/null; then
+    echo "ssh or sftp not found. Installing..."
     sudo apt update
     sudo apt install -y ssh
 fi
@@ -19,12 +17,6 @@ if ! command -v sshpass &> /dev/null; then
     echo "sshpass not found. Installing..."
     sudo apt update
     sudo apt install -y sshpass
-fi
-
-if ! command -v sftp &> /dev/null; then
-    echo "sftp not found. Installing..."
-    sudo apt update
-    sudo apt install -y openssh-client
 fi
 
 # Request user credentials
@@ -39,21 +31,22 @@ log_invalid_attempt() {
 # To copy the log file to the server using SFTP
 copy_log_to_server() {
     echo "Copying log file to server..."
-    sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o BatchMade=no -o ConnectTimeout=5 $username@SERVER_IP "exit" 2>/dev/null
-    sshpass -p "$SFTP_PASS" sftp -oBatchMode=no $SFTP_USER@$SERVER_IP <<< $"put $INVALID_ATTEMPTS_LOG"
+    # By default, SSH looks for the private key in the ~/.ssh directory therefore, we did not use the -i /path/to/private_key option.
+    ssh -o StrictHostKeyChecking=no $SFTP_USER@$SERVER_IP "mkdir -p logs"
+    sftp -oBatchMode=no $SFTP_USER@$SERVER_IP <<< "put $INVALID_ATTEMPTS_LOG logs/"
 }
 
 # Attempt login and handle invalid attempts
-while [ $ATTEMPT_COUNT -lt $MAX_ATTEMPTS ]; do 
-    sshpass -p "$password" ssh $username@$SERVER_IP "exit" 2>/dev/null
+while [ $ATTEMPT_COUNT -lt $MAX_ATTEMPTS ]; do # attempts less than max attempts allowed
+    sshpass -p "$password" ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no $username@$SERVER_IP "exit" 2>/dev/null
     # Check if login was successful
     if [ $? -eq 0 ]; then     #check exit code 
         echo -e "\nLogin successful!"
         exit 0
     else
         echo -e "\nInvalid credentials. Please try again."
-        ((ATTEMPT_COUNT++))
         log_invalid_attempt
+        ((ATTEMPT_COUNT++))
         if [ $ATTEMPT_COUNT -lt $MAX_ATTEMPTS ]; then
             read -s -p "Enter password: " password
         fi
