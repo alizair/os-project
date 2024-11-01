b56a974ea26b6850bcbcb2e8bfab1b422b453d98
Ali Zair <az2109964@qu.edu.qa>
2024-11-01 23:31:40 +0300

    updated search.sh for sending the right content and to the right email address

--- client_two/search.sh
+++ client_two/search.sh
@@ -7,13 +7,13 @@ echo -e "$file_names\n" | tee -a ./bigfile
 echo "Number of files found: $num_files" | tee -a ./bigfile
 
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-echo "Search Date: $timestamp" | tee -a ./bigfile
+echo -e "Search Date: $timestamp\n" | tee -a ./bigfile
 
-#Check if 'bigfile' is not empty and send an email
+# Check if bigfile is not empty
 if [ -s ./bigfile ]; then
     # Prepare the email content
-    mail_body="Files larger than 1M found on $timestamp:\n$(cat ./bigfile)"
+    mail_body="$(cat ./bigfile)"
     
     # Send the email to the system administrator
-    echo -e "$mail_body" | mail -s "Large Files Report" QUID@qu.edu.qa
+    echo "$mail_body" | mail -s "Large Files Report" unsecurea64@gmail.com
 fi
